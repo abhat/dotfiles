a22066fc24966c1653acbd7e9e4f2fc2d283f049
Aniket Bhat <abhat@nuagenetworks.net>
2018-03-06 12:51:28 -0800

    Add .emacs.d

---
+++ .emacs.d/elpa/company-irony-c-headers-1.0.0/company-irony-c-headers-autoloads.el
@@ -0,0 +1,29 @@
+;;; company-irony-c-headers-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (company-irony-c-headers) "company-irony-c-headers"
+;;;;;;  "company-irony-c-headers.el" (22931 18343 630645 902000))
+;;; Generated autoloads from company-irony-c-headers.el
+
+(autoload 'company-irony-c-headers "company-irony-c-headers" "\
+Company backend for C/C++ header files.  Taking COMMAND ARG IGNORED.
+
+\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("company-irony-c-headers-pkg.el") (22931
+;;;;;;  18343 705231 593000))
+
+;;;***
+
+(provide 'company-irony-c-headers-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-irony-c-headers-autoloads.el ends here
---
+++ .emacs.d/elpa/company-irony-c-headers-1.0.0/company-irony-c-headers-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company-irony-c-headers" "1.0.0" "Company mode backend for C/C++ header files with Irony" (quote ((cl-lib "0.5") (company "0.9.0") (irony "0.2.0"))))
